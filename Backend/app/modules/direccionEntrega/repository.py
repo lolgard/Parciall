0c9b19052332a59c5cf4eed35e0153e8ee48d01b
Enzo <enzocercola7@gmail.com>
2026-05-15 16:59:05 -0300

    agrege repositorio de usuario y direccion entrega

---
+++ Backend/app/modules/direccionEntrega/repository.py
@@ -0,0 +1,49 @@
+
+from datetime import datetime
+
+from sqlmodel import Session, select
+
+from app.core.repository import BaseRepository
+from app.modules.direccionEntrega.model import DireccionEntrega
+
+
+class DireccionEntregaRepository(BaseRepository):
+    def __init__(self, session:Session):
+        self.session=session
+
+    def create(self, data):
+
+        direccionEntrega = DireccionEntrega(**data.dict())
+        direccionEntrega = self.session.add(direccionEntrega)
+
+        return direccionEntrega
+    
+    def get_by_name(self,name:str)-> DireccionEntrega:
+        return self. session.exec(
+            select(DireccionEntrega).where(DireccionEntrega.bane == name)
+        ).first()
+    
+    def get_by_id(self, id:int) -> DireccionEntrega | None:
+        direccionEntrega = self.session.get(DireccionEntrega, id)
+
+        if not direccionEntrega or direccionEntrega.deleted_at is not None:
+            return None
+        return direccionEntrega
+    
+    def get_all(self) -> list[DireccionEntrega]:
+        DireccionEntregas =self.session.exec(
+            select(DireccionEntrega).where(DireccionEntrega.deleted_at == None)
+        ).all()
+        for direccionEntrega in DireccionEntregas:
+            self.session.refresh(direccionEntrega)
+        return DireccionEntregas
+    
+    def update(self,direccionEntrega:DireccionEntrega) ->DireccionEntrega:
+        self.session.add(direccionEntrega)
+        self.session.flush()
+        return direccionEntrega
+    
+    def delete(self,direccionEntrega:DireccionEntrega)-> None:
+        direccionEntrega.delete_at = datetime.utcnow(
+            self.session.add(direccionEntrega)
+        )
---
+++ Backend/app/modules/usuario/repository.py
@@ -0,0 +1,49 @@
+
+from datetime import datetime
+
+from sqlmodel import Session, select
+
+from app.core.repository import BaseRepository
+from app.modules.usuario.model import Usuario
+
+
+class UsuarioRepository(BaseRepository):
+    def __init__(self, session:Session):
+        self.session=session
+
+    def create(self, data):
+
+        usuario = Usuario(**data.dict())
+        usuario = self.session.add(usuario)
+
+        return usuario
+    
+    def get_by_name(self,name:str)-> Usuario:
+        return self. session.exec(
+            select(Usuario).where(Usuario.bane == name)
+        ).first()
+    
+    def get_by_id(self, id:int) -> Usuario | None:
+        usuario = self.session.get(Usuario, id)
+
+        if not usuario or usuario.deleted_at is not None:
+            return None
+        return usuario
+    
+    def get_all(self) -> list[Usuario]:
+        usuarios =self.session.exec(
+            select(Usuario).where(Usuario.deleted_at == None)
+        ).all()
+        for usuario in usuarios:
+            self.session.refresh(usuario)
+        return usuarios
+    
+    def update(self,usuario:Usuario) ->Usuario:
+        self.session.add(usuario)
+        self.session.flush()
+        return usuario
+    
+    def delete(self,usuario:Usuario)-> None:
+        usuario.delete_at = datetime.utcnow(
+            self.session.add(usuario)
+        )
